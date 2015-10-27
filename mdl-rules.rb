31d8030238c827ea15a88c4b6cbaa37063216104
Kirill Kornyakov <kirill.kornyakov@itseez.com>
2015-10-27 10:49:04 +0300

    Add mdl config

---
+++ mdl-rules.rb
@@ -0,0 +1,8 @@
+all
+
+exclude_rule 'MD004' # Unordered list style
+exclude_rule 'MD006' # Consider starting bulleted lists at the beginning of the line
+exclude_rule 'MD024' # Multiple headers with the same content
+exclude_rule 'MD025' # Multiple top level headers in the same document
+
+rule 'MD026', :punctuation => '.,;:!'
