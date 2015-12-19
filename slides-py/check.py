5ef967d99f6b463fe762576ed07c3d734a130d32
Kirill Kornyakov <kirill.kornyakov@itseez.com>
2015-12-18 18:07:55 -0800

    Update check.py

--- slides-py/check.py
+++ slides-py/check.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python3
+
 import os
 import glob
 import re
 import subprocess
 import traceback
 
+from functions import gglob, cat, grep
+
 root = os.path.join(subprocess.getoutput('git rev-parse --show-toplevel'), 'slides')
-
-
-def cat(name):
-    return open(name, 'r').readlines()
-
-
-def grep(text, pattern):
-    p = re.compile(pattern)
-    lines = []
-    i = 0
-    for l in text:
-        i += 1
-        if p.match(l):
-            lines += [i]
-    return lines
 
 
 def get_grep(text, pattern):
@@ -30,19 +18,8 @@ def get_grep(text, pattern):
         out += p.findall(l)
     return out
 
-
-def gglob(path, f):
-    l = glob.glob(os.path.join(path, f))
-    for folder in os.listdir(path):
-        full_path = os.path.join(path, folder)
-        if os.path.isdir(full_path):
-            l += gglob(full_path, f)
-    return l
-
-
 def check_size(name, size):
     return True if os.path.getsize(name) < size else False
-
 
 def who_am_i():
     stack = traceback.extract_stack()
