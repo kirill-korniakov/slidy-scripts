4a18a48149fbcb1def626d1a9e66a9aa4adb18f2
Kirill Kornyakov <kirill.kornyakov@itseez.com>
2016-03-11 08:42:13 +0300

    Minor cleanings of the check script

--- slides-py/check.py
+++ slides-py/check.py
@@ -10,7 +10,6 @@ from functions import gglob, cat, grep
 
 root = os.path.join(subprocess.getoutput('git rev-parse --show-toplevel'), 'slides')
 
-
 def get_grep(text, pattern):
     p = re.compile(pattern)
     out = []
@@ -21,56 +20,50 @@ def get_grep(text, pattern):
 def check_size(name, size):
     return True if os.path.getsize(name) < size else False
 
-def who_am_i():
+def print_caller_name():
     stack = traceback.extract_stack()
     _, _, caller_name, _ = stack[-2]
 
     print(" - " + caller_name)
 
-
-# ---------------------------------------------------------
-
 def check_no_tabs():
-    who_am_i()
-    err = False
+    print_caller_name()
+    error = False
     global root
     files = gglob(root, "*.md")
     for f in files:
         lines = grep(cat(f), "\t")
         if len(lines) > 0:
             print('error: in {} has tabs in {} lines'.format(f, lines))
-            err = True
-    return err
-
+            error = True
+    return error
 
 def check_empty_line_before_eof():
-    who_am_i()
-    err = False
+    print_caller_name()
+    error = False
     global root
     files = gglob(root, "*.md")
     for f in files:
         if cat(f)[-1][-1] != '\n':
             print('error: in {} has no line break in the end of file'.format(f))
-            err = True
-    return err
-
+            error = True
+    return error
 
 def check_no_trailing_spaces():
-    who_am_i()
-    err = False
+    print_caller_name()
+    error = False
     global root
     files = gglob(root, "*.md")
     for f in files:
         lines = grep(cat(f), ".*[ ]+$")
         if len(lines) > 0:
             print('error: in {} has space in the end of lines {}'.format(f, lines))
-            err = True
-    return err
-
+            error = True
+    return error
 
 def check_use_highlighting():
-    who_am_i()
-    err = False
+    print_caller_name()
+    error = False
     global root
     files = gglob(root, "*.md")
     for f in files:
@@ -81,25 +74,23 @@ def check_use_highlighting():
             print('error: {} has block code without highlighting:'.format(f))
             for b in m:
                 print('\n{}\n'.format(b))
-            err = True
-    return err
-
+            error = True
+    return error
 
 def check_all_file_size(max_size):
-    who_am_i()
-    err = False
+    print_caller_name()
+    error = False
     global root
     files = gglob(root, "*")
     for f in files:
         if not check_size(f, max_size):
             print('error: sizeof({}) >= {}'.format(f, max_size))
-            err = True
-    return err
-
+            error = True
+    return error
 
 def check_all_file_name():
-    who_am_i()
-    err = False
+    print_caller_name()
+    error = False
     global root
     files = os.listdir(root)
     for f in files:
@@ -108,14 +99,14 @@ def check_all_file_name():
             file = os.path.join(path, os.path.basename(path) + '.md')
             if not os.path.exists(file):
                 print('error: folder {} has not {}'.format(f, file))
-                err = True
-    return err
-
+                error = True
+    return error
 
 def check_all_images_are_used():
-    who_am_i()
-    err = False
+    print_caller_name()
+    error = False
     global root
+
     files = gglob(root, "*.md")
     for f in files:
         path = os.path.dirname(os.path.abspath(f))
@@ -137,14 +128,13 @@ def check_all_images_are_used():
             print("images are not used: ")
             for i in existing_images:
                 print('      - {}'.format(i))
-            err = False  # TODO: change to Truem so check fails
+            error = True  # TODO: change to Truem so check fails
 
-    return err
-
+    return error
 
 def check_all_images_exist():
-    who_am_i()
-    err = False
+    print_caller_name()
+    error = False
     global root
     files = gglob(root, "*.md")
     for f in files:
@@ -154,30 +144,28 @@ def check_all_images_exist():
             full_path = os.path.join(path, img).replace('/./', '/')
             if not os.path.exists(full_path):
                 print('error: {} does not exist'.format(full_path))
-                err = True
-    return err
-
+                error = True
+    return error
 
 def main():
     print("Starting validation process:")
-    err = False
+    error = False
 
-    err = err or check_use_highlighting()
-    err = err or check_no_trailing_spaces()
-    err = err or check_no_tabs()
-    err = err or check_empty_line_before_eof()
-    err = err or check_all_file_size(2500 * 1024)
-    err = err or check_all_file_name()
-    err = err or check_all_images_exist()
-    err = err or check_all_images_are_used()
+    error = error or check_use_highlighting()
+    error = error or check_no_trailing_spaces()
+    error = error or check_no_tabs()
+    error = error or check_empty_line_before_eof()
+    error = error or check_all_file_size(2500 * 1024)
+    error = error or check_all_file_name()
+    error = error or check_all_images_exist()
+    error = error or check_all_images_are_used()
 
-    if err:
+    if error:
         print("FAIL: validation finished with errors")
         return 1
     else:
         print("SUCCESS: validation finished without errors")
         return 0
-
 
 if __name__ == '__main__':
     exit(main())
